ced523d0d439489116f81066a942a9e5009c2f1b
yuris99 <mose1207@gmail.com>
2021-08-23 23:42:01 +0900

    build setting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
+    devServer: {
+        disableHostCheck: true
+    },
     publicPath: process.env.NODE_ENV === 'production' ? '/hdgegg/' : '/',
     outputDir: 'docs',
 };
